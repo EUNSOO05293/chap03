5b0097ce82b7e2cec418ab393e6c869bf5a8fe60
eunsoo <ehfl0489@gmail.com>
2025-04-03 18:08:22 +0900

    push

--- Assignment03/assign03.c
+++ Assignment03/assign03.c
@@ -1,3 +1,10 @@
+/* 파일명 Assignment-03.c
+ * 작성자: 이은수
+ * 내용: 질량과 높이를 입력받아 위치 에너지를 구하는 프로그램을 작성
+ * 날짜: 2025.04.02
+  버전: v1.0
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
--- Assignment04/assign04.c
+++ Assignment04/assign04.c
@@ -1,3 +1,10 @@
+/* 파일명 Assignment-04.c
+ * 작성자: 이은수
+ * 내용: 물체에 작용한 힘의 크기와 힘의 방향으로 이동한 거리를 입력받아 한 일의 양을 구하는 프로그램 작성
+ * 날짜: 2025.04.02
+  버전: v1.0
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
@@ -15,7 +22,7 @@ void Calculate_work(void)
 	
 	work = force * distance;
 
-	printf("일의 양: %.2lf", work);
+	printf("일의 양: %.2lf", force * distance);
 
 	return;
 }
